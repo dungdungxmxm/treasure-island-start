cd4644fdca5f2f04a510bc4adc3b81e8da9342f1
DungNgo <74999590+dungdungxmxm@users.noreply.github.com>
2022-11-25 11:07:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" of "right". ')
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" of "right". ').lower()
 
 if choice1 == "left":
-  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ')
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ').lower()
   if choice2 == "wait":
-    choice3 = input("You arrive at the island unharmed. There is a house with a doors. One red, one yellow and one blue. Which color do you choose? ")
+    choice3 = input("You arrive at the island unharmed. There is a house with a doors. One red, one yellow and one blue. Which color do you choose? ").lower()
     if choice3 == "red":
       print("It's a room full of fire. Game Over.")
     elif choice3 == "yellow":
